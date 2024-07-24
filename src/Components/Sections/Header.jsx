f4635d51501cdbbc8a462500d6d5c7f4c34d153c
sampath <sampathgoud451@gmail.com>
2024-07-24 08:01:17 +0530

    resolved

--- src/Components/Sections/Header.jsx
+++ src/Components/Sections/Header.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 
-
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid d-flex justify-content-between align-items-center">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           <img
             src="" // Replace with your logo path
             alt="BookYourGround"
-            
           />
         </a>
-       
+
         <form className="d-flex flex-grow-1 mx-2" role="search">
           <input
             className="form-control me-2"
@@ -21,9 +19,8 @@ const Header = () => {
             aria-label="Search"
           />
           <button className="btn btn-outline-success me-2" type="submit">Search</button>
-          <button className="btn btn-outline-success" type="submit">SignIn</button>
+          <a className="btn btn-outline-success" href="/signin">Sign In</a>
         </form>
-       
       </div>
     </nav>
   );
