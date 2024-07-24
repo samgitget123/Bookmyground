e770ecaaa5715c4a3b97e3f00fb773d6ed509ffb
sampath <sampathgoud451@gmail.com>
2024-07-24 07:19:54 +0530

    adding new project

---
+++ src/Components/Sections/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid d-flex justify-content-between align-items-center">
+        <a className="navbar-brand" href="#">
+          <img
+            src="path/to/your/logo.png" // Replace with your logo path
+            alt="BookYourGround"
+            
+          />
+        </a>
+       
+        <form className="d-flex flex-grow-1 mx-2" role="search">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success me-2" type="submit">Search</button>
+          <button className="btn btn-outline-success" type="submit">SignIn</button>
+        </form>
+       
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
