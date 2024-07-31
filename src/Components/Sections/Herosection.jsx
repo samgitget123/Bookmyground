e2c0896d30e0b95b7687f341f5b439dba1d2b873
sampath <sampathgoud451@gmail.com>
2024-07-31 15:01:59 +0530

    resloved the config file lines

--- src/Components/Sections/Herosection.jsx
+++ src/Components/Sections/Herosection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectCity, selectArea, setSearchQuery } from '../../Features/citySlice';
+import { selectCity, selectArea } from '../../Features/citySlice';
 import banner from '../../Images/turf.jpeg';
 
 const Herosection = () => {
